b213bb5bd933c1e757ffb72864d0c271a609d642
Nishal NM <106329531+nishal-nm@users.noreply.github.com>
2025-01-18 01:58:29 +0530

    Test v0.21

--- src/data/my-data.js
+++ src/data/my-data.js
@@ -1,7 +1,7 @@
 const myData = {
   img: "nishalnm.png",
   name: "Nishal NM",
-  job: "Student Engineered",
+  job: "Student Engineers",
   linkedin: "http://www.linkedin.com/in/nishal-nm",
   github: "https://github.com/nishal-nm",
   facebook: "https://www.facebook.com/ashikmalu.cpr",
