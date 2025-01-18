9ceb846f8654b6d43d6da38d4374efd9ead02bac
nishal-nm <21gcs20@meaec.edu.in>
2025-01-18 19:58:39 +0530

    change phone

--- src/data/my-data.js
+++ src/data/my-data.js
@@ -9,7 +9,7 @@ const myData = {
     "I'm a Computer Science undergraduate with a passion for technology, especially in Python, web development, and database management. I love working on real-world projects and solving problems through code. In my free time, I enjoy exploring new technologies, learning new skills, and building things that make a difference. I'm always eager to grow and take on new challenges in the tech world.",
   location: "Malappuram",
   email: "nishalshalu17@gmail.com",
-  phone: 6235544220,
+  phone: "+91 6235544220",
   address: {
     house: "Narimadakkal",
     po: "Venniyur",
