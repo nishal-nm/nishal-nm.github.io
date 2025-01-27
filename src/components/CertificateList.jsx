312fe7388abb247ddf4ed9e1c54f5076dd68e2a0
nishal-nm <21gcs20@meaec.edu.in>
2025-01-28 00:53:34 +0530

    exp no edit

--- src/components/CertificateList.jsx
+++ src/components/CertificateList.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import certificateData from "../data/certificate-data";
+import experienceData from "../data/experience-data";
 import Certificate from "./Certificate";
 import Extender from "./ui/Extender";
 
-const CertificateList = () => {
+const noOfExp = experienceData.length;
+
+function CertificateList() {
   return (
     <section className="latest section">
       <div className="section-inner shadow-sm rounded">
         <h2 className="heading">Certificates</h2>
         <div className="content">
-          <Extender data={certificateData} initial={2}>
+          <Extender data={certificateData} initial={noOfExp}>
             {(shows) => {
               return certificateData.slice(0, shows).map((certificate) => {
                 return (
@@ -27,6 +30,6 @@ const CertificateList = () => {
       </div>
     </section>
   );
-};
+}
 
 export default CertificateList;
