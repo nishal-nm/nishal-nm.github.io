32dcc3d57a0c128989042805f6d9f615d674c8ac
nishal-nm <21gcs20@meaec.edu.in>
2025-01-30 23:25:29 +0530

    convert data files from js to json

--- src/data/resume-data.js
+++
@@ -1,7 +0,0 @@
-const resumeData = {
-  pdf: "assets/pdf/resumes/resume.pdf",
-  ats: "assets/pdf/resumes/resume_ats.txt",
-  word: "assets/pdf/resumes/resume.docx",
-};
-
-export default resumeData;
